a78dac59fcb300089c3cc69f6d1cf0cc4412242d
engageintellect <jc9361@gmail.com>
2023-09-01 18:06:01 -0700

    init svelte search component

---
+++ src/components/FormatDate.svelte
@@ -0,0 +1,14 @@
+<!-- FormattedDate.svelte -->
+<script>
+  export let date; // Input property for the date string
+</script>
+
+<span>
+  {#if date}
+    {new Date(date).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    })}
+  {/if}
+</span>
---
+++ src/components/SearchFilter.svelte
@@ -0,0 +1,67 @@
+<!-- SearchFilter.svelte -->
+<script lang="ts">
+  let searchInput = "";
+  let handleSearch: any;
+  let allItems: any = [];
+  let inputElement: HTMLInputElement;
+  import FormatDate from "./FormatDate.svelte";
+
+  export { searchInput, handleSearch, allItems };
+
+  handleSearch = (e: any) => {
+    searchInput = e.target.value;
+  };
+
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    inputElement.focus();
+  });
+</script>
+
+<div class="flex flex-col max-w-4xl px-2 mx-auto p-2">
+  <input
+    type="text"
+    id="search"
+    class="border border-gray-300 rounded p-2 flex-grow sticky top-12"
+    placeholder="Search"
+    on:input={handleSearch}
+    bind:value={searchInput}
+    bind:this={inputElement}
+    autofocus
+  />
+
+  {#each allItems as item}
+    {#if item.data.title.toLowerCase().includes(searchInput.toLowerCase())}
+      <div>
+        <hr />
+        <a href={`/project/${item.slug}/`}>
+          <div class="my-5">
+            <div class="text-xs font-thin mb-2">
+              <span class="border p-1 rounded">{item.data.category}</span>
+            </div>
+            <div class="flex">
+              <div class="text-xl font-bold">{item.data.title}</div>
+            </div>
+            <div class="text-gray-500 text-sm">
+              <FormatDate date={item.data.pubDate} />
+            </div>
+
+            {#if item.data.tags}
+              <div class="mt-2 flex flex-wrap gap-1">
+                {#each item.data.tags as tag}
+                  <div
+                    class="bg-gray-200 text-xs rounded px-2 py-1 text-gray-700"
+                  >
+                    #{tag}
+                  </div>
+                {/each}
+              </div>
+            {/if}
+          </div>
+        </a>
+        <hr />
+      </div>
+    {/if}
+  {/each}
+</div>
