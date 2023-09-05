4d5f8bffea67520ad5db416595fa3f1a8cbf3ee6
engageintellect <jc9361@gmail.com>
2023-09-04 20:43:46 -0700

    scrambler

--- src/components/SvelteTypewriter.svelte
+++ src/components/SvelteTypewriter.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-  class="text-xl text-neutral-900/70 flex items-center bg-red-500 m-0"
+  class="text-xl text-neutral-900/70 flex items-center bg-red-500 m-0 w-full"
   use:scramble={{
     interval: 300,
     scrambleDuration: 1000,
