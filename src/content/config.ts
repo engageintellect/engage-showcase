d0ad4962301b24ffa08ecb1db5d63afa851e4643
engageintellect <jc9361@gmail.com>
2023-09-06 12:11:39 -0700

    init

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ const project = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
-    description: z.string(),
+    description: z.string().optional(),
     // Transform string to Date object
     pubDate: z
       .string()
